458debaa76a1ea91390d835d691491a1de00d6c0
rjsrnr <133875265+rjsrnr@users.noreply.github.com>
2023-11-26 19:28:50 +0900

    [Bronze V] Title: 윤년, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.py
+++ 백준/Bronze/2753. 윤년/윤년.py
@@ -1,11 +1,6 @@
 year = int(input())
 
-if year % 4 == 0 :
-    if year % 400 ==0 :
-        print(1)
-    elif year % 100 ==0 :
-        print(0)
-    else :
-        print(1)
-else :
+if year%4 == 0 and year%100 != 0 or year%400 == 0 :
+    print(1)
+else:
     print(0)
