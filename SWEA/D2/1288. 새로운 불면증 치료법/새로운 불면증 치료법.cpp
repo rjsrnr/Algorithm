1861cdd6be9f1dccb461445a70bd0c345ff0aa87
박건국 <133875265+rjsrnr@users.noreply.github.com>
2024-06-20 18:44:20 +0900

    [D2] Title: 새로운 불면증 치료법, Time: 15 ms, Memory: 13,548 KB -BaekjoonHub

--- SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.cpp
+++ SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.cpp
@@ -3,9 +3,8 @@
 
 using namespace std;
 
-int N;
-int goal = (1 << 10) - 1;
-int ans;
+int N, ans, cnt;
+int map[10];
 
 
 int main() {
@@ -13,17 +12,22 @@ int main() {
 	cin >> T;
 	for (int tc = 1; tc <= T; tc++) {
 		cin >> N;
-		
+		for (int i = 0; i < 10; i++) {
+			map[i] = 0;
+		}
+		cnt = 0;
 		int time = 1;
-		int result = 0;
 		while (1) {
 			string cmp = to_string(N * time);
 			for (int i = 0; i < cmp.length(); i++) {
 				int tmp = cmp[i] - '0';
-				result = result | (1 << tmp);
+				if (map[tmp] == 0) {
+					map[tmp] = 1;
+					cnt++;
+				}
 			}
 
-			if (result == goal){
+			if (cnt == 10){
 				ans = stoi(cmp);
 				break;
 			}
