9e97a1c9f2ceb5eb15f12bd7e4ec59ab11820629
박건국 <133875265+rjsrnr@users.noreply.github.com>
2024-10-28 21:20:32 +0900

    [Silver I] Title: 1, 2, 3 더하기 5, Time: 0 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/15990. 1， 2， 3 더하기 5/1， 2， 3 더하기 5.cc
+++ 백준/Silver/15990. 1， 2， 3 더하기 5/1， 2， 3 더하기 5.cc
@@ -2,29 +2,27 @@
 
 using namespace std;
 
-long long dp[100001], arr[100001][4];
+long long dp[100001], arr[4][100001];
 
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	arr[1][1] = arr[2][2] = arr[3][1] = arr[3][2] = arr[3][3] = 1;
+	arr[1][1] = arr[2][2] = arr[1][3] = arr[2][3] = arr[3][3] = 1;
 	dp[1] = dp[2] = 1, dp[3] = 3;
-
 	for (int i = 4; i <= 100000; i++) {
-		arr[i][1] = (arr[i - 1][2] + arr[i - 1][3]) % 1000000009;
-		arr[i][2] = (arr[i - 2][1] + arr[i - 2][3]) % 1000000009;
-		arr[i][3] = (arr[i - 3][1] + arr[i - 3][2]) % 1000000009;
-		dp[i] = (arr[i][1] + arr[i][2] + arr[i][3]) % 1000000009;
+		arr[1][i] = (arr[2][i - 1] + arr[3][i - 1]) % 1000000009;
+		arr[2][i] = (arr[1][i - 2] + arr[3][i - 2]) % 1000000009;
+		arr[3][i] = (arr[1][i - 3] + arr[2][i - 3]) % 1000000009;
+		dp[i] = (arr[1][i] + arr[2][i] + arr[3][i]) % 1000000009;
 	}
 
 	int N;
 	cin >> N;
-
-	for (int tc = 0; tc < N; tc++) {
-		int a;
-		cin >> a;
-		cout << dp[a] << '\n';
+	for (int i = 0; i < N; i++) {
+		int tmp;
+		cin >> tmp;
+		cout << dp[tmp] << '\n';
 	}
-	int de = 1;
+
 	return 0;
 }
