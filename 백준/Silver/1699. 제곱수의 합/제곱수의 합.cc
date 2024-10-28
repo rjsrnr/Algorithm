32b08658ae8abd4c36a1b01e3cbfdc5d458f4409
박건국 <133875265+rjsrnr@users.noreply.github.com>
2024-10-28 20:12:35 +0900

    [Silver II] Title: 제곱수의 합, Time: 60 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int arr[322], dp[100005];
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int N;
+    cin >> N;
+    for (int i = 0; i < 100001; i++)
+        dp[i] = 21e8;
+    for (int i = 1; i <= 320; i++) {
+        arr[i] = i * i;
+        dp[arr[i]] = 1;
+    }
+    for (int i = 0; i <= N; i++) {
+        for (int j = 1; j <= 320; j++) {
+            int cnt = dp[i] + 1;
+            int idx = arr[j] + i;
+            if (idx > N)
+                break;
+            dp[idx] = min(cnt, dp[idx]);
+        }
+    }
+    cout << dp[N];
+    return 0;
+
+}
